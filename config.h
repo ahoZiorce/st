78344dade070af7c71e764faa88e6311d1e95e04
Alejandro Sior <aho@sior.be>
2020-04-24 10:02:20 +0200

    Removed the alpha patch

--- config.h
+++ config.h
@@ -87,9 +87,6 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-/* bg opacity */
-float alpha = 0.9;
-
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -193,7 +190,6 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
-        { "alpha",        FLOAT,   &alpha },
 };
 
 /*
